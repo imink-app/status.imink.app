591c08217f1f2eb855ebc59476477f04fe27e49c
Jone Wang <i@jone.wang>
2022-03-25 23:11:32 +0800

    update

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,7 +9,7 @@ const fs = require("fs");
         await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
-        await execa("echo", ["'status.imink.app'", ">", "CNAME"]);
+        await execa("echo", ["'status.imink.app'", ">", "./" + folderName + "/CNAME"]);
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
