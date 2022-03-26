bf0de4294107849734d663e54b84d11b0184893f
Jone Wang <i@jone.wang>
2022-03-26 11:32:36 +0800

    Add error and degradation status icons.

--- src/components/ServiceList.vue
+++ src/components/ServiceList.vue
@@ -13,14 +13,16 @@
       <tbody>
       <tr v-for="item in items" :key="item.id" class="h-20">
         <th class="content-center">
-          <check-circle-icon class="h-8 w-8 text-green-500"/>
+          <x-circle-icon v-if="item.status.isDegraded" class="h-8 w-8 text-red-500"/>
+          <exclamation-circle-icon v-else-if="item.status.hasErrors || item.status.hasFailures" class="h-8 w-8 text-yellow-400"/>
+          <check-circle-icon v-else class="h-8 w-8 text-green-500"/>
         </th>
         <td>
           <div class="text-lg font-medium font-mono">{{ item.name }}</div>
         </td>
         <td>
           <div class="grid grid-rows-1 grid-flow-col items-end">
-            <div v-for="data in item.chartData" :key="data.id" class="w-1 bg-green-500 mr-0.5 tooltip" v-bind:class="{'bg-yellow-500': data.isDegraded, 'bg-red-500': data.hasErrors || data.hasFailures}" v-bind:style="{height: (0.5 + 1.5 * data.v) + 'rem'}" v-bind:data-tip="data.responseTime + 'ms'"></div>
+            <div v-for="data in item.chartData" :key="data.id" class="w-1 bg-green-500 mr-0.5 tooltip" v-bind:class="{'bg-yellow-400': data.isDegraded, 'bg-red-500': data.hasErrors || data.hasFailures}" v-bind:style="{height: (0.5 + 1.5 * data.v) + 'rem'}" v-bind:data-tip="data.responseTime + 'ms'"></div>
           </div>
         </td>
         <td class="font-mono">{{ item['status'].metrics['1dSuccessRatio'] }}%</td>
@@ -32,11 +34,11 @@
 </template>
 
 <script>
-import {CheckCircleIcon} from '@heroicons/vue/solid'
+import {CheckCircleIcon, XCircleIcon, ExclamationCircleIcon} from '@heroicons/vue/solid'
 
 export default {
   name: "ServiceList",
-  components: {CheckCircleIcon},
+  components: {CheckCircleIcon, XCircleIcon, ExclamationCircleIcon},
   props: {
     items: Array
   }
